40584a07851b0fe0ecab40b32e2230f8d22824c0
Ira Miller <public@iramiller.com>
2018-06-29 17:28:13 -0500

    init

--- README.md
+++ README.md
@@ -1,68 +1,36 @@
-# git-config
+# guld-cli
 
-Guld configuration helper manages git config files.
-
-### Example Output
-
-```
-$ guld-config list
-user.username=isysd
-user.signingkey=C7EA0E59D0660BF6848614B6441BDDD420F44729
-commit.gpgsign=true
-alias.pushall=!git remote | xargs -L1 -I R git push R $2
-host.github=isysd
-host.bitbucket=isysd
-host.gitlab=isysd
-core.testing4=true
-core.repositoryformatversion=0
-core.filemode=true
-core.bare=false
-core.logallrefupdates=true
-```
+Universal wrapper for guld commands. Discovers `guld-*` commands and allows them to be called as `guld *` from the main script.
 
 ### Install
 
 ```
-npm i -g guld-config
+npm i -g guld-cli
 ```
 
 ### Usage
 
-```
-// async
-// assume it's empty
-var cfg = await getConfig('local') // {}
-cfg.core.test = true
-setConfig('cfg.core.test', true)
-var cfg = await getConfig('local') // {core: {test: true}}
-```
-
-##### Node
-
-```
-const { guldName, getConfig, setConfig, unsetConfig } = require('guld-config')
-```
 
 ##### CLI
 
 ```
-  Usage: guld-config [options] [command]
+  Usage: guld [options] [command]
+
+  Guld standardized Command Line Interface (CLI) for git.
 
   Options:
 
-    -V, --version             output the version number
-    --global                  Use the global config file
-    --local                   Use the local config file
-    --system                  Use the system config file.
-    -f, --file <config-file>  Use the given config file.
-    -h, --help                output usage information
+    -V, --version      output the version number
+    -u, --user         The user name to set up.
+    -r, --recipient    The recipient of a message or transaction.
+    -f, --fingerprint  The PGP fingerprint to sign with.
+    -h, --help         output usage information
 
   Commands:
 
-    name                      Get the guld name of the current user.
-    get <key>                 Get a config by key.
-    set <key> <value>         Set a config key to the given value.
-    unset                     Get the distro, if linux OS.
-    list                      List all config key/value pairs.
+    config             Manage git config files the guld way.
+    env                Guld environment detection module.
+    git                Guld standardized Command Line Interface (CLI) for git.
+    help [cmd]         display help for [cmd]
 ```
 
--- cli.js
+++ cli.js
@@ -1,116 +1,35 @@
 #!/usr/bin/env node
-// eslint-disable-file no-console
-const { guldName, getConfig, setConfig, unsetConfig } = require('./index.js')
-const flat = require('flat')
-const _get = require('lodash.get')
 const program = require('commander')
-const VERSION = require('./package.json').version
+const pkg = require('./package.json')
+const VERSION = pkg.version
+const NAME = Object.keys(pkg.bin)[0]
+const config = require('guld-config')
+
+const COMMANDS = {
+  'config': ['Manage git config files the guld way.', {isDefault: true}],
+  'env': ['Guld environment detection module.'],
+  'git': ['Guld standardized Command Line Interface (CLI) for git.']
+}
 
 program
-  // .usage('<key> Get a config by key (alias of get)')
-  // .usage('<key> <value> Set a config key to the given value (alias of set)')
+  .name('guld')
   .version(VERSION)
-  .option('--global', 'Use the global config file')
-  .option('--local', 'Use the local config file')
-  .option('--system', 'Use the system config file.')
-  .option('-f, --file <config-file>', 'Use the given config file.')
-program
-  .command('name')
-  .description('Get the guld name of the current user.')
-program
-  .command('get <key>')
-  .description('Get a config by key.')
-program
-  .command('set <key> <value>')
-  .description('Set a config key to the given value.')
-program
-  .command('unset')
-  .description('Get the distro, if linux OS.')
-program
-  .command('list')
-  .description('List all config key/value pairs.')
+  .description('Guld decentralized internet CLI.')
+  .option('-u, --user', 'The user name to set up.')
+  .option('-r, --recipient', 'The recipient of a message or transaction.')
+  .option('-f, --fingerprint', 'The PGP fingerprint to sign with.')
+  // .option('-q, --quiet', '')
+
+for (var cmd in COMMANDS) {
+  var cmds = COMMANDS[cmd]
+  var desc = cmds.shift()
+  if (cmds.length > 0) {
+    program.command(cmd, desc, ...cmds).description(desc)
+  } else {
+    program.command(cmd, desc).description(desc)
+  }
+}
 
 program.parse(process.argv)
 
-var scope
-if (program.global) scope = 'global'
-else if (program.system) scope = 'system'
-else if (program.local) scope = 'local'
-else if (program.file) scope = program.file
-else scope = 'merged'
-var cmd
-if (program.args.length === 0) cmd = 'name'
-else if (program.commands.map(c => c._name).indexOf(program.args[0]) !== -1) cmd = program.args.shift()
-/* eslint-disable no-console */
-switch (cmd) {
-  case 'name':
-    guldName().then(console.log)
-    break
-  case 'get':
-    getConfig(scope).then(c => {
-      var val = _get(c, program.args[0])
-      if (val) console.log(val)
-    })
-    break
-  case 'set':
-    setConfig(program.args[0], program.args[1], scope).then(console.log)
-    break
-  case 'unset':
-    unsetConfig(program.args[0], scope)
-    break
-  case 'list':
-    getConfig(scope).then(conf => {
-      var cfg = flat(conf)
-      for (var c in cfg) {
-        console.log(`${c}=${cfg[c]}`)
-      }
-    })
-    break
-  /* case 'setup':
-    inquirer
-      .prompt([
-        {
-          name: guldname,
-          type: input,
-          message: "What is your individual guld name?",
-          default: await guldName()
-        },
-        {
-          name: fullname,
-          type: input,
-          message: "What is your public full name? (optional)",
-          default: await guldName()
-        },
-        {
-          name: guldmail,
-          type: input,
-          message: "What is your public email address? (optional)"
-        },
-        {
-          name: signingkey,
-          type: input,
-          message: "Which of these is your PGP key id? (blank for group)"
-        }
-      ])
-      .then(answers => {
-        answers.commit = {gpgsign: true}
-        setupConfig(answers).then(c => {
-          var cfg = flat(c)
-          for (var c in cfg) {
-            console.log(`${c}=${cfg[c]}`)
-          }
-        })
-      })
-    break */
-  default:
-    if (program.args.length === 1) {
-      getConfig(scope).then(c => {
-        var val = _get(c, program.args[0])
-        if (val) console.log(val)
-      })
-    } else if (program.args.length === 2) {
-      setConfig(program.args[0], program.args[1], scope).then(console.log)
-    }
-    break
-}
-/* eslint-enable no-console */
+module.exports = program
